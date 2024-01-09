5f179921d1646c12e95d6dd5eab58134f27fc725
akri <alecjoseph.dev@gmail.com>
2024-01-10 02:18:27 +0800

    added theme for header and footer

---
+++ wp-content/themes/goalkeepers/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package GoalKeepers
+ */
+
+?>
+	<!-- footer area -->
+	<footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 footer-logo">
+                        <a href="index.html">
+                            <img src="<?php bloginfo('template_directory');?>/img/Logo-GoalKeepers.svg" alt="GoalKeepers Logo"><img src="<?php bloginfo('template_directory');?>/img/Text-Logo-GoalKeepers.svg" alt="GoalKeepers Logo in Text">
+                        </a>
+                    </div>
+                    <div class="col-lg-6 socials">
+                        <div class="col-lg-4"><img src="<?php bloginfo('template_directory');?>/img/social-fb.svg" alt="facebook icon"><span>@goalkeepersteamph</span></div>
+                        <div class="col-lg-3"><img src="<?php bloginfo('template_directory');?>/img/social-phone.svg" alt="phone icon"><span>09954781200</span></div>
+                        <div class="col-lg-5"><img src="<?php bloginfo('template_directory');?>/img/social-email.svg" alt="email icon"><span>goal.keepersteam@gmail.com</span></div>
+                    </div>
+                    <div class="col-lg-3 copyright"><span>&copy; 2024 GOALKEEPERS. All rights reserved. </span></div>
+                </div>
+            </div>
+        </footer>
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ wp-content/themes/goalkeepers/header.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package GoalKeepers
+ */
+$page = 'header';
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> id="wrapper-div">
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'goalkeepers' ); ?></a>
+	<!-- navigation bar -->
+	<header>
+        <nav class="container">
+            <div class="row">
+                <div class="col-lg-3 logo">
+                    <a href="index.html">
+                        <img src="<?php bloginfo('template_directory');?>/img/Logo-GoalKeepers.svg" alt="GoalKeepers Logo"><img src="<?php bloginfo('template_directory');?>/img/Text-Logo-GoalKeepers.svg" alt="GoalKeepers Logo in Text">
+                    </a>
+                </div>
+                <div class="col-lg-7 nav-links">
+                    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></li>
+                </div>
+                <div class="col-lg-2 cta-fill"><a href="index.html" class="cta-btn">Contact Us</a></div>
+            </div>
+        </nav>
+    </header>    
