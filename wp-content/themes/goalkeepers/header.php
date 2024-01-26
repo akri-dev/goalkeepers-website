b5325393fd155fb98dd8f87417990162d7b3b649
akri <alecjoseph.dev@gmail.com>
2024-01-27 07:24:29 +0800

    converted static responsive navbar

--- wp-content/themes/goalkeepers/header.php
+++ wp-content/themes/goalkeepers/header.php
@@ -25,17 +25,26 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'goalkeepers' ); ?></a>
 	<!-- navigation bar -->
 	<header>
-        <nav class="container">
-            <div class="row">
-                <div class="col-lg-3 logo">
+        <nav class="navbar navbar-expand-lg container justify-content-center">
+            <div class="container-fluid row">
+                <div class="navbar-brand col-lg-3 logo justify-content-between">
                     <a href="index.php">
-                        <img src="<?php bloginfo('template_directory');?>/img/Logo-GoalKeepers.svg" alt="GoalKeepers Logo"><img src="<?php bloginfo('template_directory');?>/img/Text-Logo-GoalKeepers.svg" alt="GoalKeepers Logo in Text">
+                    <img src="<?php bloginfo('template_directory');?>/img/Logo-GoalKeepers.svg" alt="GoalKeepers Logo"><img src="<?php bloginfo('template_directory');?>/img/Text-Logo-GoalKeepers.svg" alt="GoalKeepers Logo in Text">
                     </a>
+                    <button class="navbar-toggler col-lg-1" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                  </button>
                 </div>
-                <div class="col-lg-7 nav-links">
-                    <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></li>
-                </div>
-                <div class="col-lg-2 cta-fill"><a href="index.html" class="cta-btn">Contact Us</a></div>
+                
+                <div class="collapse navbar-collapse col-lg-9 nav-links justify-content-end" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?></li>
+                    </ul>
+                    <div class="cta-fill"><a href="index.html" class="cta-btn">Contact Us</a></div>
+                  </div>
+                  
+                </div>  
+              </nav>
             </div>
         </nav>
     </header>    
--- wp-content/themes/goalkeepers/index.php
+++ wp-content/themes/goalkeepers/index.php
@@ -15,7 +15,27 @@ get_header();
 ?>
 
 	<main id="primary" class="site-main">
-		<div class="hero-section"></div>
+	<div class="hero-section">
+                    <div class="row h-100 align-items-center">
+                            <div class="col-12 hero-text h-100 align-items-center">
+                                <div class="header-tagline d-flex justify-content-left">
+                                    <h1 class="display-1 text-left">Strengthening Teams<br>For Success</h1>
+                                </div>
+                                <div class="learn-more">
+                                    <div class="row d-flex justify-content-center">
+                                        <p>Drive significant performance improvement, foster collaboration, and lead with impact. Experience the transformative difference for your organization.</p>
+                                    </div>
+                                    <div class="row d-flex justify-content-center"><a href="about-us.php">Learn More</a>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="row cards justify-content-center">
+                        <div class="col-3 card-group">A</div>
+                        <div class="col-3 card-group">B</div>
+                        <div class="col-3 card-group">C</div>
+                    </div>
+            </div>
 	</main><!-- #main -->
 
 <?php
